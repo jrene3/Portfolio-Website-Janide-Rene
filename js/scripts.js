66f6bb71815b16419ebe6591205f524822b5c07d
jrene3 <janidelrene@gmail.com>
2022-07-18 14:37:17 -0400

    Adjusted pages based on user feedback

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
-console.log("Hello & Welcome to My Portfolio");
+console.log('Hello & Welcome to My Portfolio')
 
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
+console.log('linter-js-standard')
